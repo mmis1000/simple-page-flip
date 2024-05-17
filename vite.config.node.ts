7040a426dd091df2a8971a72f6ce10447e9ea144
Mmis1000 <mmis1000@yahoo.com.tw>
2024-05-18 00:47:54 +0800

    add ci

---
+++ vite.config.node.ts
@@ -0,0 +1,33 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+  plugins: [dts({ rollupTypes: true })],
+  build: {
+    minify: false,
+    sourcemap: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/utils/effects.ts'),
+      name: 'MyLib',
+      // the proper extensions will be added
+      fileName: 'simple-page-flip',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['gl-matrix'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          ['gl-matrix']: 'glMatrix',
+        },
+      },
+      
+    },
+  },
+})
