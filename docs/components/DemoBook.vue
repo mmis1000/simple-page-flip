c2294c3149afefcd2121c810ebb48c3c2f83bdef
mmis1000 <mmis10002@gmail.com>
2025-01-12 17:59:32 +0800

    simplify demo dom

--- docs/components/DemoBook.vue
+++ docs/components/DemoBook.vue
@@ -1,28 +1,27 @@
 <template>
   <div :class="{ right: onRight }">
     <div class="wrap" :style="{ '--WIDTH': width, '--HEIGHT': height }">
-      <div class="item-layer layer-under">
-        <div class="item">
-          <div class="text-placeholder">Text Text</div>
-        </div>
+      <div class="item layer-under">
+        <div class="text-placeholder">Text Text</div>
       </div>
-      <div class="item-layer layer-flip-front">
-        <div class="item" :style="asStyleValue(effect.flipFront)">
-          The<br />
-          &nbsp&nbspBook Title
-        </div>
+      <div
+        class="item layer-flip-front"
+        :style="asStyleValue(effect.flipFront)"
+      >
+        The<br />
+        &nbsp&nbspBook Title
       </div>
-      <div class="item-layer layer-shadow">
-        <div class="item" :style="asStyleValue(effect.flipShadow)"></div>
+      <div
+        class="item layer-shadow"
+        :style="asStyleValue(effect.flipShadow)"
+      ></div>
+      <div class="item layer-flip-back" :style="asStyleValue(effect.flipBack)">
+        <div class="text-placeholder">Text Text</div>
       </div>
-      <div class="item-layer layer-flip-back">
-        <div class="item" :style="asStyleValue(effect.flipBack)">
-          <div class="text-placeholder">Text Text</div>
-        </div>
-      </div>
-      <div class="item-layer layer-effect">
-        <div class="item" :style="asStyleValue(effect.flipEffect)"></div>
-      </div>
+      <div
+        class="item layer-effect"
+        :style="asStyleValue(effect.flipEffect)"
+      ></div>
     </div>
   </div>
 </template>
@@ -49,7 +48,7 @@ defineProps({
   },
 });
 
-const asStyleValue = (v: any): Partial<StyleValue> => v
+const asStyleValue = (v: any): Partial<StyleValue> => v;
 </script>
 <style lang="css" scoped>
 .wrap {
@@ -64,43 +63,37 @@ const asStyleValue = (v: any): Partial<StyleValue> => v
   position: relative;
 }
 
-.item-layer {
+.item {
   position: absolute;
   height: calc(var(--scale-px, 1px) * var(--HEIGHT));
   width: calc(var(--scale-px, 1px) * var(--WIDTH));
   left: 0;
   top: 0;
-  pointer-events: none;
-}
-
-.right .item-layer {
-  transform: translateX(100%);
-}
-
-.item {
-  height: calc(var(--scale-px, 1px) * var(--HEIGHT));
-  width: calc(var(--scale-px, 1px) * var(--WIDTH));
   background: #4f4fbb;
   color: white;
   box-sizing: border-box;
-  padding: calc(100% / var(--HEIGHT) * 32);
+  padding: calc(100% / var(--WIDTH) / 2 * 24);
   pointer-events: all;
 }
 
-.layer-flip-back .item,
-.layer-under .item {
+.right .item {
+  left: calc(var(--scale-px, 1px) * var(--WIDTH));
+}
+
+.layer-flip-back.item,
+.layer-under.item {
   background: #e7e7e7;
   color: #333;
   border: 1px solid black;
 }
 
-.layer-flip-front .item {
+.layer-flip-front.item {
   font-size: calc(var(--scale-px, 1px) * 24);
   line-height: 1.5;
 }
 
-.layer-under .item,
-.layer-flip-back .item {
+.layer-under.item,
+.layer-flip-back.item {
   display: flex;
   align-items: stretch;
   justify-content: stretch;
